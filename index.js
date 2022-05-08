d94e159dbb51052c3b24e5694346c08cdc4fb0ab
Abu-Sahad <abusahad603@gmail.com>
2022-05-08 08:29:23 +0600

    delete api

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
-//use middleware
+
 app.use(cors());
 app.use(express.json());
 
@@ -15,33 +15,46 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.USER}:${process.env.USER_PASS}@cluster0.bd2wi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-    try{
+async function run() {
+    try {
 
         await client.connect()
         const laptopCollection = client.db('hunter-laptop').collection('items')
-        
-        // get all item api
-        app.get('/laptops',async(req,res)=>{
-           const query = {}
-           const cursor = laptopCollection.find(query)
-           const laptops =  await cursor.toArray()
-           res.send(laptops)
+
+        // all item api
+        app.get('/laptops', async (req, res) => {
+            const query = {}
+            const cursor = laptopCollection.find(query)
+            const laptops = await cursor.toArray()
+            res.send(laptops)
         })
-        // get single item api
-        app.get('/laptops/:id',async (req,res)=>{
+        // single item api
+        app.get('/laptops/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id:ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const laptop = await laptopCollection.findOne(query)
             res.send(laptop)
 
         })
 
+        // post/create item api
+        app.post('/laptop', async (req, res) => {
+            const laptop = req.body
+            const result = await laptopCollection.insertOne(laptop)
+            res.send(result)
+        })
+        // delete item api      
+        app.delete('/laptop/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await laptopCollection.deleteOne(query)
+            res.send(result)
+        })
+        // update api 
         
 
-
     }
-    finally{
+    finally {
 
     }
 }
@@ -51,7 +64,7 @@ run()
 
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('hello')
 })
 
